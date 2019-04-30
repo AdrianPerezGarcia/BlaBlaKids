77f3c73e387b8f67698aeb0abe2c9d92d1bae8eb
Adrián <apereg24@estudiantes.unileon.es>
2019-04-30 20:18:07 +0200

    Segundo commit para lo mismo

--- src/es/unileon/prg1/blablakid/BlaBlaKidsApp.java
+++ src/es/unileon/prg1/blablakid/BlaBlaKidsApp.java
@@ -4,23 +4,6 @@ package es.unileon.prg1.blablakid;
  *
  */
 public class BlaBlaKidsApp {
-	/**
-	 * Metodo principal de la aplicacion que comprueba los parametros e inicia la aplicacion
-	 */
-	public static void main(String[] args){
-		int numberOfKids;
-			if(args.length != 1){
-				System.out.println("Error: Argumentos incorrectos");
-			}
-			else{
-				try {
-					numberOfKids = Integer.parseInt(args[0]);
-				}catch(NumberFormatException e) {
-					System.out.println("Error: Caracter introducido como argumento no valido");
-				}
-				
-			}
-	}
 
 }
 
