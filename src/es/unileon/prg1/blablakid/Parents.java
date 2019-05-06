3edc83abd53db83eed15afbae7cf7132ce3d7ffb
Pablo Javier Barrio <pbarrn00@estudiantes.unileon.es>
2019-05-06 03:24:58 +0200

    Añadidos metodos add y remove ademas de search mañana completo doc

--- src/es/unileon/prg1/blablakid/Parents.java
+++ src/es/unileon/prg1/blablakid/Parents.java
@@ -37,30 +37,51 @@ public class Parents {
 	 * @param parent
 	 */
 	
-	public void addParent(Parent parent, Kids kids)throws ParentException{
+	public boolean addParent(Parent parent)throws ParentException{
 		// TODO Auto-generated method stub
+		boolean added = false;
+		if (isIncluded(parent)) {
+			throw new ParentException("Error, parent is already included");
+		}
+		else {
+			if (this.next >= this.size) {
+				throw new ParentException("Error, the list of parents is full!");
+			}
+			else {
+				this.parents[this.next] = parent;
+				this.next++;
+				added = true;
+			}
+		}		
+		return added;
 	}
 	
 	/**
 	 * @return boolean
 	 * @param parent
 	 */
-	public boolean removeParent(Parent parent) {
-		return false;
-		// TODO Auto-generated method stub
-	}	
-
-	/**
-	 * @return boolean
-	 * @par
-	 */
-	private boolean isIncluded(String name) {
-		boolean result =  false;
+	public boolean removeParent(Parent parent) throws ParentException{
+		boolean removed = false;
 		
-		return result;
+		if (isIncluded(parent)) {
+			
+			int position = inWhichPosIs(parent);
+			parents[position] = null;
+			position++;
+			
+			while (parents[position] != null) {
+				parents[(position -1)] = parents [position];
+				position++;
+			}
+			parents[position-1]=null;
+			next--;
+		}
+		else {
+			throw new ParentException("Error, the parent is not in the list");
+		}
+		return removed;
 		// TODO Auto-generated method stub
 	}
-
 
 	/**
 	 * @return the length
@@ -94,8 +115,11 @@ public class Parents {
 	
 	/**
 	 * 
+	 * Method that shows in which position of the array is the parent
+	 * 
 	 * @param parent
-	 * @return
+	 * @return the position of the array 
+	 * 
 	 */
 	private int inWhichPosIs(Parent parent){
 		int count = 0;
@@ -116,6 +140,7 @@ public class Parents {
 	 *  
 	 *  @param parent
 	 *  @return result
+	 *  
 	 */
 	public boolean isIncluded(Parent parent) {
 		boolean resultado = false;
@@ -126,6 +151,37 @@ public class Parents {
 			}
 		}
 		return resultado;
+	}
+	
+	/**
+	 * 
+	 *  Method that search a parent in the array of parents
+	 *  
+	 *  @param name
+	 *  @return the parent or null if the parent isn't on the array
+	 *  
+	 */
+	
+	public Parent searchParent(String name) {
+		int count = 0;
+		boolean found = false;
+		
+		while (count < this.parents.length && !found) {
+			if (this.parents[count].getName().equals(name)) {
+				found = true;
+			}
+			else {
+				count++;
+			}
+		}
+		
+		if (found == true) {
+			return this.parents[count];
+		}
+		else {
+			return null;
+		}
+		
 	}
 
 
