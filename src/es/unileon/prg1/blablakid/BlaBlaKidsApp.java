9c553f3ea57cfd2430bfcb227ac4137304e01b5e
pbarrn00 <pbarrn00@estudiantes.unileon.es>
2019-05-09 14:09:19 +0200

    Añadidos doc en blablakidsapp y getters ademas de añadido ridetest y ridestests

--- src/es/unileon/prg1/blablakid/BlaBlaKidsApp.java
+++ src/es/unileon/prg1/blablakid/BlaBlaKidsApp.java
@@ -5,15 +5,69 @@ package es.unileon.prg1.blablakid;
  */
 public class BlaBlaKidsApp {
 	
+	/**
+	 * 
+	 */
 	private int numberOfKids;
-	private int numberOfParents = numberOfKids * 2;
+	/**
+	 * 
+	 * Number of parents in BlablakidsApp
+	 * @see int 
+	 * 
+	 */
+	private int numberOfParents;
 	
+	/**
+	 * 
+	 *	Array of kids in BlaBlaKidsApp
+	 *	@see Kids
+	 *
+	 */
 	private Kids kids;
+	
+	/**
+	 * 
+	 *	 Array of parents in BlaBlaKidsApp
+	 * 	 @see Parents
+	 * 
+	 */
 	private Parents parents;
 	
 	public BlaBlaKidsApp(int numberKids){
 		
+		this.numberOfKids = numberKids;
+		this.numberOfParents = numberKids * 2;
+		
 	}
+
+	/**
+	 * @return the numberOfKids
+	 */
+	public int getNumberOfKids() {
+		return numberOfKids;
+	}
+
+	/**
+	 * @return the numberOfParents
+	 */
+	public int getNumberOfParents() {
+		return numberOfParents;
+	}
+
+	/**
+	 * @return the kids
+	 */
+	public Kids getKids() {
+		return kids;
+	}
+
+	/**
+	 * @return the parents
+	 */
+	public Parents getParents() {
+		return parents;
+	}
+
 	
 	/*
 	public void add(Parent parent, Kids kids) {
