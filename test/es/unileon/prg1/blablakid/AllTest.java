41336b5e661d354acbc48f3071eab92c8193e4f0
hcastc00 <hcastc00@estudiantes.unileon.es>
2019-05-13 22:57:05 +0200

    Corregido build para que haga clean, compile, compile test, testee y se ejecute en ese orden. Arreglado tambien AllTests.java para incluir todos los test

--- test/es/unileon/prg1/blablakid/AllTest.java
+++ test/es/unileon/prg1/blablakid/AllTest.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ KidsTest.class })
+@SuiteClasses({ KidsTest.class, HourTest.class, KidTest.class, ParentsTest.class, ParentTest.class, PlaceTest.class, RideTest.class, RidesTest.class })
 public class AllTest {
 
 }
