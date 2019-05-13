4bc1adbf23f8d1353038bdb09cc2bf58aa12e85f
pbarrn00 <pbarrn00@estudiantes.unileon.es>
2019-05-13 23:03:52 +0200

    Acabada RideTest, falta pasarla 100 %

--- test/es/unileon/prg1/blablakid/RideTest.java
+++ test/es/unileon/prg1/blablakid/RideTest.java
@@ -1,5 +1,6 @@
 package es.unileon.prg1.blablakid;
 
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -31,9 +32,29 @@ public class RideTest {
 	}
 	
 	@Test
-	public void testIsSame() {
+	public void testIsSame() throws HourException, DayException, KidException, RideException {
 		
+		Kids niños = new Kids(2);
+		Kid daniel = new Kid("Daniel");
+		Kid pablo = new Kid("Pablo");
+		niños.add(daniel);
+		niños.add(pablo);		
+			Parent pedro = new Parent("Pedro",niños, 3);
+			Place startPlace = new Place("Palomera");
+		Hour startTimeActivity = new Hour(18,00);
+		Hour endTimeActivity = new Hour(20,00);
+		WeekDays day = WeekDays.MONDAY;
+		Day today = new Day(day);
+			Activity baloncesto = new Activity("Baloncesto", startPlace, today, startTimeActivity, endTimeActivity);
+			Place endPlace = new Place("Casa");
+			Hour startTime = new Hour(20,00);
+			Hour endTime = new Hour(21,00);
+		
+		Ride viaje2 = new Ride(pedro,baloncesto,startPlace,endPlace,startTime,endTime,false);
+		
+		assertTrue(this.viaje.isSame(viaje2));
 	}
+	
 	
 	
 }
