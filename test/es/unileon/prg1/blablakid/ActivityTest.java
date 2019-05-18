5caca42d1667dfde43bc63ffceb0097c75709752
hcastc00 <hcastc00@estudiantes.unileon.es>
2019-05-19 01:40:18 +0200

    Completado ActivityTest excepto toString

--- test/es/unileon/prg1/blablakid/ActivityTest.java
+++ test/es/unileon/prg1/blablakid/ActivityTest.java
@@ -4,6 +4,7 @@
 package es.unileon.prg1.blablakid;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,67 +14,84 @@ import org.junit.Test;
 public class ActivityTest {
 	
 	private Activity actividad;
+	private Ride beforeRide;
+	private Ride afterRide;
+	private String name;
+	private Place palomera;
+	private Place casa;
+	private Day day;
+	private Hour startTime;
+	private Hour endTime;
+	private Hour beforeRideStart;
+	private Hour beforeRideEnd;
+	private Hour afterRideStart;
+	private Hour afterRideEnd;
+	
+	
 	@Before
-	public void setUp()throws ActivityException, DayException, HourException{
-		String name = new String("Baloncesto");
-		Place place = new Place("Palomera");
-		Day day = new Day(WeekDays.MONDAY);
-		Hour startTime = new Hour(15,00);
-		Hour endTime = new Hour(16,30);
+	public void setUp()throws ActivityException, DayException, HourException, RideException{
+		this.name = new String("Baloncesto");
+		this.palomera = new Place("Palomera");
+		this.casa = new Place("Casa");
+		this.day = new Day(WeekDays.MONDAY);
+		this.startTime = new Hour(15,00);
+		this.endTime = new Hour(16,00);
+		this.beforeRideStart = new Hour(14,30);
+		this.beforeRideEnd = new Hour(15,00);
+		this.afterRideStart = new Hour (16,00);
+		this.afterRideEnd = new Hour(16,30);
+		
+		this.actividad = new Activity(name,palomera,day,startTime,endTime);
+		this.beforeRide = new Ride(casa,palomera,beforeRideStart,beforeRideEnd);
+		this.afterRide = new Ride(palomera,casa,afterRideStart,afterRideEnd);
+		
 	}
 	@Test
-	void testSetBeforeRide() {
+	public void testSetGetBeforeRide() {
+		this.actividad.setBeforeRide(beforeRide);
+		assertTrue(this.actividad.getBeforeRide().isSame(beforeRide));
+	}
+
+	@Test
+	public void testSetGetAfterRide() {
+		this.actividad.setAfterRide(afterRide);
+		assertTrue(this.actividad.getAfterRide().isSame(afterRide));
+	}
+
+	@Test
+	public void testIsSame() {
+		assertTrue(this.actividad.isSame(this.actividad));
+	
+	}
+
+	@Test
+	public void testGetName() {
+		assertTrue(this.actividad.getName().equals("Baloncesto"));
+	}
+
+	@Test
+	public void testGetPlace() {
+		assertTrue(this.actividad.getPlace().getPlace().equals("Palomera"));
 	}
 	
 	@Test
-	void testGetBeforeRide() {
-
-	}
-
-
-	@Test
-	void testGetAfterRide() {
-		fail("Not yet implemented");
+	public void testGetDay() {
+		assertEquals(0,this.actividad.getDay().getNumDay());
 	}
 
 	@Test
-	void testSetAfterRide() {
-		fail("Not yet implemented");
+	public void testGetStartTime() {
+		assertTrue(this.actividad.getStartTime().isSame(this.startTime));
 	}
 
 	@Test
-	void testIsSame() {
-		fail("Not yet implemented");
+	public void testGetEndTime() {
+		assertTrue(this.actividad.getEndTime().isSame(this.endTime));
 	}
 
 	@Test
-	void testGetName() {
-		fail("Not yet implemented");
+	public void testToString() {
+		//TODO
 	}
-
-	@Test
-	void testGetPlace() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetDay() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetStartTime() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetEndTime() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testToString() {
-		fail("Not yet implemented");
-	}
-
+	 
 }
