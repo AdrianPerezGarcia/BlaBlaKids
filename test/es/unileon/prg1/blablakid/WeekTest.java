cd8c7d979ba3bac96cdde07445b0fe506face07b
pbarrn00 <pbarrn00@estudiantes.unileon.es>
2019-05-19 02:16:48 +0200

    Creada clase WeekTest

---
+++ test/es/unileon/prg1/blablakid/WeekTest.java
@@ -0,0 +1,14 @@
+package es.unileon.prg1.blablakid;
+
+/**
+ * @author 
+ *
+ */
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WeekTest {
+
+}
