eebb40a5c588da30e8c41c6864ddebffd761f33a
Adrián <apereg24@estudiantes.unileon.es>
2019-05-28 21:10:26 +0200

    A medias la docu de activity (prevencion de merge)

--- src/es/unileon/prg1/blablakid/Activity.java
+++ src/es/unileon/prg1/blablakid/Activity.java
@@ -1,56 +1,80 @@
 package es.unileon.prg1.blablakid;
+ /**
+  * 
+  * Class that create an activity that will be managed by Activities  
+  * 
+  * @author Pablo Bayon
+  *
+  **/
 
+/*Import logs libraries
+ import org.apache.logging.log4j.Logger; import
+ org.apache.logging.log4j.LogManager; */
 
-/**
- * @author Pablo Bayon
- *
- */
 public class Activity {
-
 	/**
+	 * 
 	 * Name of the activity
+	 * 
 	 */
 	private String name;
 
 	/**
+	 * 
 	 * Where the activity takes place
+	 * 
 	 */
 	private Place place;
 
 	/**
+	 * 
 	 * Day of the activity
+	 * 
 	 */
 	private Day day;
 
 	/**
+	 * 
 	 * Hour at which the activity starts
+	 * 
 	 */
 	private Hour startTime;
 
 	/**
+	 * 
 	 * Hour at which the activity ends
+	 * 
 	 */
 	private Hour endTime;
 
 	/**
+	 * 
 	 * Ride in which the kid arrives to the activity
+	 * 
 	 */
 	private Ride beforeRide;
 
 	/**
+	 * 
 	 * Ride in which the kid leaves the activity
+	 * 
 	 */
 	private Ride afterRide;
 
 	/**
-	 * Constructor que recibe como parametros el nombre de la actividad, el lugar,
-	 * el dia, ninyo, y la hora a la que empieza y acaba la actividad
 	 * 
-	 * @param name
+	 * Creates a activity with its name, place, day and times of start and end
+	 * 
+	 * @param name Activity identifier
+	 * 
 	 * @param place
+	 * 
 	 * @param day
+	 * 
 	 * @param startTime
+	 * 
 	 * @param endTime
+	 * 
 	 */
 	public Activity(String name, Place place, Day day, Hour startTime, Hour endTime) {
 		this.name = name;
@@ -60,25 +84,43 @@ public class Activity {
 		this.endTime = endTime;
 	}
 
+	/**
+	 * 
+	 * Getter of the previous ride to the activity
+	 * 
+	 * @return Ride previous to the activity
+	 * 
+	 */
 	public Ride getBeforeRide() {
 		return beforeRide;
 	}
 
+	/**
+	 * 
+	 * Getter of the ride after to the activity
+	 * 
+	 * @return Ride later the activity
+	 * 
+	 */
 	public Ride getAfterRide() {
 		return afterRide;
 	}
 	
 	/**
+	 * 
 	 * Method that sets a ride as a afterRide, beforeRide or doesn't set it if it's an invalid one
 	 * 
-	 * @param ride
+	 * @param ride to 
+
 	 * @return true if the ride is valid, false if not
-	 * @throws RideException
+	 * 
+	 * @throws RideException if the ride is not valid in its context
+	 * 
 	 */
 	public boolean setRides(Ride ride) throws RideException{
 		boolean set = false;
 		if(this.place.isSame(ride.getEndPlace())) {
-			//Se trata de un beforeRide, por lo que compruebo las horas para no llegar tarde
+			//If it is an beforeRide, i check the hours to do not be late
 			if(this.startTime.isHigher(ride.getEndTime())) {
 				this.beforeRide = ride;
 				set = true;
@@ -86,7 +128,7 @@ public class Activity {
 				throw new RideException("The ride arrives late to the activity.\n");
 			}
 		} else if(this.place.isSame(ride.getStartPlace())) {
-			//Se trata de un afterRide, compruebo que no salga antes de que acabe la actividad
+			//If it is an afterRide, i check the hours because it cannot start before the activity end
 			if (this.endTime.isLower(ride.getStartTime())) {
 				this.afterRide = ride;
 				set = true;
@@ -101,29 +143,53 @@ public class Activity {
 
 
 	/**
+	 * 
 	 * Method that checks if two objects of type Activity are the same
 	 * 
-	 * @param activity
+	 * @param activity2 to compare if it same
+	 * 
 	 * @return true if the activity is the same, false if not
+	 * 
 	 */
-	public boolean isSame(Activity activity) {
+	public boolean isSame(Activity activity2) {
 		boolean salida = false;
-		if (this.name.equals(activity.getName()) && this.place.isSame(activity.getPlace())
-				&& this.day.isSame(activity.getDay()) && this.startTime.isSame(activity.getStartTime())
-				&& this.endTime.isSame(activity.getEndTime())) {
+		if (this.name.equals(activity2.getName()) && this.place.isSame(activity2.getPlace())
+				&& this.day.isSame(activity2.getDay()) && this.startTime.isSame(activity2.getStartTime())
+				&& this.endTime.isSame(activity2.getEndTime())) {
 			salida = true;
 		}
 		return salida;
 	}
 
+	/**
+	 * 
+	 * Getter of the activity identifier
+	 * 
+	 * @return a String with the name of the activity
+	 * 
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * 
+	 * Getter of the place when the activity takes place
+	 * 
+	 * @return a type place object where the activity takes place
+	 * 
+	 */
 	public Place getPlace() {
 		return place;
 	}
 
+	/**
+	 * 
+	 * Getter of the day when the activity will take place
+	 * 
+	 * @return a type day object when the activity will take place
+	 * 
+	 */
 	public Day getDay() {
 		return day;
 	}
